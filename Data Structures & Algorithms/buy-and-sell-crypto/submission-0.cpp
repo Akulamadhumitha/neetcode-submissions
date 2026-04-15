133526d23e4c6f7ee0e9e628ae1333d22ed05e09
Akulamadhumitha <akulamadhumitha11@gmail.com>
2026-04-15 14:35:11 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int max_profit=0;
+        int buy=prices[0];
+        for(int i=1;i<n;i++){
+            if(prices[i]<buy){
+                buy=prices[i];
+            }
+            max_profit=max(max_profit,abs(buy-prices[i]));
+        }
+        return max_profit;
+    }
+};
